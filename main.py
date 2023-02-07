7c2d9fc9b8b79b0392b064d3b5f52aa50802b136
blnd2112 <121508968+blnd2112@users.noreply.github.com>
2023-02-07 15:04:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from PIL import Image
 
 
 # Data to be written to Deta Base
+image = Image.open('b.png')
+
+st.image(image, caption='4S by blnd')
 with st.form("form"):
     Name = st.text_input("Your name")
     ph = st.text_input("Your Phone number")
@@ -17,9 +20,7 @@ deta = Deta(st.secrets["deta_key"])
 # Create a new database "example-db"
 # If you need a new database, just use another name.
 db = deta.Base("bb")
-image = Image.open('b.png')
 
-st.image(image, caption='4S by blnd')
 # If the user clicked the submit button,
 # write the data from the form to the database.
 # You can store any data you want here. Just modify that dictionary below (the entries between the {}).
