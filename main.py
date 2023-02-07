18e0f36b47ef22c90ed569ffc64b06d3e37657cd
blnd2112 <121508968+blnd2112@users.noreply.github.com>
2023-02-07 15:02:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from deta import Deta
-from PIL import Image
+
 
 
 # Data to be written to Deta Base
@@ -16,8 +16,7 @@ deta = Deta(st.secrets["deta_key"])
 # Create a new database "example-db"
 # If you need a new database, just use another name.
 db = deta.Base("bb")
-db = deta.Base("bb")
-image = Image.open('b.png')
+st.write("https://cdn.discordapp.com/attachments/847070786639495208/1072487501442322522/b.png")
 # If the user clicked the submit button,
 # write the data from the form to the database.
 # You can store any data you want here. Just modify that dictionary below (the entries between the {}).
